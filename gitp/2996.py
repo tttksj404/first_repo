c0d4b586eaf5c31a760eae0258c1c1c1ecc5d19c
tttksj <tttksj@naver.com>
2026-01-25 18:32:35 +0900

    test01 풀이성공

---
+++ gitp/2996.py
@@ -0,0 +1,16 @@
+data_1 = 'qweqwYadnOyjnsaU4trwg asjnaAn245krRmkfE 42grTasdnHasdnvEasdn asdevadnBasdanEsdkqefqefvaSasdqaeeqqvedwt5hfbsdT24tewfd'
+'''
+예시코드
+arr = [1, 2, 3, 4, 5]
+for num in arr:
+    print(num, end='')
+출력결과 : 12345
+'''
+# 아래에 코드를 작성하시오.
+
+
+
+print()
+data_2 = '걉파반샤팝다푸거맥파바자들퍼바배들밥샵파누타히매니배사바파힘다브사부힙헤베내테치대내'
+arr = []
+# 아래에 코드를 작성하시오.
