b47de48503e35f4af53291b1d3bf001084f85ede
김성진 <tttksj@gmail.com>
2026-02-03 12:13:57 +0900

    swea 풀이

--- gitp/복습.py
+++ gitp/복습.py
@@ -1,16 +1,2 @@
-user_data1 = {
-    'id': 'jungssafy5',
-    'password': '1q2w3e4r',
-}
-
-a='jungssafy5'
-c = []
-for b in a:
-    c.append(b)
-
-print(c)
-f= len(c)
-print(f)
-g= int(f)-1
-#print(c[len(c)])
-print(c[g])
+numbers= list(map(int,input()))
+print(numbers)
