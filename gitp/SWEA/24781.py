4931e7fc87bfc0e8e7652f2c21b76566357d8865
김성진 <tttksj@gmail.com>
2026-02-03 17:33:26 +0900

    24781 다시해보는데 있는 코드 살릴 수 있으면 살려보기

--- gitp/SWEA/24781.py
+++ gitp/SWEA/24781.py
@@ -1,3 +1,30 @@
+def fly_catcher(case,y,x):
+    for a in range(y-1,y+2):
+        case.append(numbers[a][x])
+    for b in range(x-1,x+2):
+        case.append(numbers[y][b])
+    case.remove(numbers[y][x])
+    return sum(case)
+    
+
+    
+
+
+T = int(input())
+for w in range(1,T+1):
+    N = int(input())
+    for i in range(N): #행 만큼 반복 
+        numbers = list(map(int, input().split()))
+    storage = []
+    max_killer = max(storage)
+    for r in range(N):
+        if r>=1 and r<=N-1:
+            for c in range(N):
+                 if c>=1 and c<=N-1:
+                      if fly_catcher(storage,r,c) == max_killer:
+                          pass
+                          
+                      
 
     
 
