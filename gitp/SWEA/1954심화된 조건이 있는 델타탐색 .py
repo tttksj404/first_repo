b71078247aa2594f59f8e9b56b90dff99d67dbbc
2Limit <tidhtidhdldl@naver.com>
2026-02-05 17:33:53 +0900

    21940 풀이 집가서 ㄱ

--- gitp/SWEA/1954심화된 조건이 있는 델타탐색 .py
+++ gitp/SWEA/1954심화된 조건이 있는 델타탐색 .py
@@ -16,16 +16,16 @@ for w in range(1,T+1):
     dr = [0,1,0,-1] #우->하->좌->상 행값
     dc = [1,0,-1,0] #열값
 
-    r,c = 0,0
-    dist = 0
-
+    r,c = 0,0 #시작값 설정
+    dist = 0 #방향 설정
+#다음 좌표가 갈 수 있는 곳인지 체크만 하는 곳이고
     for num in range(1, N*N+1): #1,2,3...N까지 숫자 넣기 
         snaill[r][c] = num
-        nr = r+ dr[dist]
+        nr = r+ dr[dist] 
         nc = c+ dc[dist]
-
+#여기가 그 방향대로 직진한다는 것임 
         if nr<0 or nr>=N or nc<0 or nc>=N or snaill[nr][nc] != 0: #snail이 0으로 이뤄졌는데 거기에 숫자 하나씩 들어갈때 
-            #0이 아닌 다른 숫자가 채워지기에 
+            #0이 아닌 다른 숫자가 채워지기에 #막혔을때 방향전환되는거 
             dist = (dist+1)%4 #0,1,2,3 반복시 우하좌상인데 각각 넣으면 1,2,3,0 되서 하좌상우로 반복됨 
             nr = r+dr[dist]
             nc = c+dc[dist]
